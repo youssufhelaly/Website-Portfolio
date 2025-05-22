b75b2aacfdf3b9d9ff483a7a95b4333c1ff02af7
Youssuf Helaly <youssufhelaly@gmail.com>
2025-05-22 05:22:21 -0400

    Adding divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 // src/app/page.tsx
 import Hero from "@/components/Hero"
 import About from "@/components/About"
-// ← remove Experience & Education imports
 import Timeline from "@/components/Exp-Edu"
 import Projects from "@/components/Projects"
 import Divider from "@/components/Divider"
+import TechStack from "@/components/TechStack"
 
 export default function Page() {
   return (
     <>
       <Hero />
+      <Divider/>
       <About />
+       <Divider/>
       <Timeline /> 
+       <Divider/>
+      <TechStack />
+      <Divider/>
       <Projects />
     </>
   )
--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,6 +1,6 @@
 // src/components/Divider.tsx
 export default function Divider() {
   return (
-    <div className="w-24 h-1 bg-cyan-500 my-12 mx-auto rounded"></div>
+    <div className="w-24 h-1 bg-cyan-700 my-12 mx-auto rounded"></div>
   )
 }
