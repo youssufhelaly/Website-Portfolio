177477205edc6bfd3d9e9e59ce67699ddcecbb35
Youssuf Helaly <youssufhelaly@gmail.com>
2025-06-13 01:57:22 -0400

    remove about

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,6 @@ import { motion } from "framer-motion"
 export default function Navbar() {
   const menuItems = [
     { name: "Home", href: "#home" },
-    { name: "About", href: "#about" },
     { name: "Projects", href: "#projects" },
     { name: "Testimonials", href: "#testimonials" },
     { name: "Contact", href: "mailto:youssufhelaly@gmail.com" },
